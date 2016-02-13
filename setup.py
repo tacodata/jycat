cd1cb72a236bcd2da7c5bd031727364fc10528b5
Greg Fausak <Greg.Fausak@VerizonWireless.com>
2016-02-13 08:11:42 -0600

    added argparse_config to install_requries

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[],
+    install_requires=['argparse_config'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
